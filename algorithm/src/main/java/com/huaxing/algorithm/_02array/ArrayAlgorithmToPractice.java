453fff5fb8e8bc2cfd7e444e795fe3ff9adcd301
yaoguangxing <yaoguangxing@geostar.com.cn>
2021-01-03 18:15:14 +0800

    数组习题3:旋转数组

--- algorithm/src/main/java/com/huaxing/algorithm/_02array/ArrayAlgorithmToPractice.java
+++ algorithm/src/main/java/com/huaxing/algorithm/_02array/ArrayAlgorithmToPractice.java
@@ -1,5 +1,6 @@
 package com.huaxing.algorithm._02array;
 
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -46,10 +47,102 @@ public class ArrayAlgorithmToPractice {
          * 示例 3:输入: [7,6,4,3,1]     输出: 0
          * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
          */
-        int[] nums2 = {7, 1, 5, 3, 6, 4};
-        int profit = ArrayAlgorithmToPractice.maxProfit2(nums2);
+        nums = new int[]{7, 1, 5, 3, 6, 4};
+        int profit = ArrayAlgorithmToPractice.maxProfit2(nums);
         System.out.println("profit = " + profit);
         //---------------- 习题2:买卖股票的最佳时机（贪心算法） end -----------------------------
+
+        //---------------- 习题3:旋转数组（三种方式） begin ---------------------
+        //方式1：直接旋转
+        nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        ArrayAlgorithmToPractice.rotate1(nums, 3);
+        //方式2：
+        nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        ArrayAlgorithmToPractice.rotate2(nums, 3);
+        //---------------- 习题3:旋转数组（三种方式） end -----------------------
+    }
+
+    /**
+     * TODO 旋转数组    方式1：直接旋转
+     * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+     * 说明:
+     * 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+     * 要求使用空间复杂度为 O(1) 的 原地 算法。
+     * 复杂度分析：
+     * 时间复杂度：O(k*n)。n为数组的元素，k为旋转次数
+     * 空间复杂度：O(1)。常数位的空间复杂度，没有额外的空间被使用
+     * <p>
+     * 作者：华星详谈
+     * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2skh7/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @param k
+     */
+    public static void rotate1(int[] nums, int k) {
+        //temp:临时存储，previous：之前的值
+        int temp, previous;
+        for (int i = 0; i < k; i++) {
+            //保存数组最后一个元素
+            previous = nums[nums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
+                //把当前位的值先暂时放入temp中
+                temp = nums[j];
+                //当前位的值替换为最后位的值
+                nums[j] = previous;
+                //把当前位的值最为下一次循环的最后位值
+                previous = temp;
+            }
+        }
+        System.out.println("方式一 直接旋转：nums = " + JSON.toJSONString(nums));
+    }
+
+    /**
+     * TODO 旋转数组    方式2：反转数组
+     * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+     * 说明:
+     * 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+     * 要求使用空间复杂度为 O(1) 的 原地 算法。
+     * 复杂度分析：
+     * 时间复杂度：O(n)。n为数组的元素。n 个元素被反转了总共 3 次。
+     * 空间复杂度：O(1)。常数位的空间复杂度，没有额外的空间被使用
+     * <p>
+     * 作者：华星详谈
+     * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2skh7/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @param k
+     */
+    public static void rotate2(int[] nums, int k) {
+        // k = k%数组的长度，eg k=3%7=3；
+        k %= nums.length;
+        //反转所有数字
+        reversal(nums, 0, nums.length - 1);
+        //反转前 k 个数字
+        reversal(nums, 0, k - 1);
+        //反转后 n-k 个数字
+        reversal(nums, k, nums.length - 1);
+        System.out.println("方式二 使用反转：nums = " + JSON.toJSONString(nums));
+    }
+
+    /**
+     * 反转数组
+     *
+     * @param nums
+     * @param start 数组开始下标
+     * @param end   数组结束下标
+     */
+    private static void reversal(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 
     /**
