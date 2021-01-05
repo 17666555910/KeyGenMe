bb04bcc870a076639945836cc9f03131f5fe48e9
yaoguangxing <yaoguangxing@geostar.com.cn>
2021-01-05 21:57:05 +0800

    数组习题4:存在重复元素

--- algorithm/src/main/java/com/huaxing/algorithm/_02array/ArrayAlgorithmToPractice.java
+++ algorithm/src/main/java/com/huaxing/algorithm/_02array/ArrayAlgorithmToPractice.java
@@ -3,6 +3,12 @@ package com.huaxing.algorithm._02array;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * @Description
  * @author: 姚广星
@@ -60,6 +66,60 @@ public class ArrayAlgorithmToPractice {
         nums = new int[]{1, 2, 3, 4, 5, 6, 7};
         ArrayAlgorithmToPractice.rotate2(nums, 3);
         //---------------- 习题3:旋转数组（三种方式） end -----------------------
+
+        //---------------- 习题4:存在重复元素 begin ---------------------
+        /**
+         * 示例 1:    输入: [1,2,3,1]   输出: true
+         * 示例 2:    输入: [1,2,3,4]   输出: false
+         * 示例 3:    输入: [1,1,1,3,3,4,3,2,4,2]   输出: true
+         */
+        nums = new int[]{1,2,3,1};
+        System.out.println("习题4:存在重复元素 方式一: " + ArrayAlgorithmToPractice.containsDuplicate(nums));
+        //---------------- 习题4:存在重复元素 end -----------------------
+    }
+
+    /**
+     * TODO 存在重复元素  方式1：使用set集合的方式  LeetCode执行用时：5 ms
+     * 给定一个整数数组，判断是否存在重复元素。
+     * 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+     *
+     * 作者：华星详谈
+     * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x248f5/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param nums
+     * @return
+     */
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet();
+        for(int i=0;i < nums.length;i++){
+            if(!set.add(nums[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * TODO 存在重复元素  方式2：排序之后判断相邻元素的是否相等     LeetCode执行用时：3 ms
+     * 给定一个整数数组，判断是否存在重复元素。
+     * 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+     *
+     * 作者：华星详谈
+     * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x248f5/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param nums
+     * @return
+     */
+    public static boolean containsDuplicate2(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1;i <nums.length;i++){
+            if(nums[i-1] == nums[i]){
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
