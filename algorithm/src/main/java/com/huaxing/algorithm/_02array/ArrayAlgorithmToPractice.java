70c527033629a980a8a3016ae2673865735659e1
华星详谈 <javaygx@163.com>
2021-01-14 22:55:05 +0800

    习题7:加一  解题思路：digits[i]++ ,然后 digits[i] 取余 10，如果能够被整除，说明进一位，如果不能够被整除则直接返回

--- algorithm/src/main/java/com/huaxing/algorithm/_02array/ArrayAlgorithmToPractice.java
+++ algorithm/src/main/java/com/huaxing/algorithm/_02array/ArrayAlgorithmToPractice.java
@@ -114,6 +114,7 @@ public class ArrayAlgorithmToPractice {
          */
         nums = new int[]{7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 6};
         System.out.println("习题7:加一 方式一: " + JSONObject.toJSONString(ArrayAlgorithmToPractice.plusOne(nums)));
+        System.out.println("习题7:加一 方式二: " + JSONObject.toJSONString(ArrayAlgorithmToPractice.plusOne2(nums)));
         //---------------- 习题7:加一 end --------------------
     }
 
