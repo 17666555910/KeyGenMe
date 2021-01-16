2856450db44baa9f0849e1e45237b74712ccfffd
华星详谈 <javaygx@163.com>
2021-01-16 16:54:13 +0800

    习题9:两数之和

--- algorithm/src/main/java/com/huaxing/algorithm/_02array/ArrayAlgorithmToPractice.java
+++ algorithm/src/main/java/com/huaxing/algorithm/_02array/ArrayAlgorithmToPractice.java
@@ -118,11 +118,74 @@ public class ArrayAlgorithmToPractice {
         //---------------- 习题7:加一 end --------------------
 
         //---------------- 习题8 移动零 begin ------------------
+        //示例: 输入: [0,1,0,3,12] 输出: [1,3,12,0,0]
         nums = new int[]{0, 1, 0, 3, 12};
         ArrayAlgorithmToPractice.moveZeroes(nums);
-
         //---------------- 习题8 移动零 end ------------------
+
+        //---------------- 习题9 两数之和 begin ------------------
+        /**
+         * 示例 1：输入：nums = [2,7,11,15], target = 9 输出：[0,1] 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+         * 示例 2：输入：nums = [3,2,4], target = 6 输出：[1,2]
+         * 示例 3：输入：nums = [3,3], target = 6 输出：[0,1]
+         */
+        nums = new int[]{3, 3};
+        System.out.println("习题9:两数之和 方式一: " + JSONObject.toJSONString(ArrayAlgorithmToPractice.twoSum(nums, 6)));
+        System.out.println("习题9:两数之和 方式二: " + JSONObject.toJSONString(ArrayAlgorithmToPractice.twoSum2(nums, 6)));
+        //---------------- 习题9 两数之和 begin ------------------
     }
+
+    /**
+     * TODO 两数之和：方式一：解题思路：数组转list，判断target-当前元素的值在list中是否存在，若存在返回对应的下标，若不存在，进入下一个循环
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     * 你可以按任意顺序返回答案。
+     * <p>
+     * 作者：华星详谈
+     *
+     * @return
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        //解题思路：数组转list，判断target-当前元素的值在list中是否存在，若存在返回对应的下标，若不存在，进入下一个循环
+        List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        int[] result = new int[nums.length];
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int i1 = target - nums[i];
+            if (list.contains(i1)) {
+                if (list.indexOf(nums[i]) != list.lastIndexOf(i1)) {
+                    result[0] = list.indexOf(nums[i]);
+                    result[1] = list.lastIndexOf(i1);
+                    index++;
+                    break;
+                }
+            }
+        }
+        return Arrays.copyOfRange(result, 0, index + 1);
+    }
+
+    /**
+     * TODO 两数之和：方式二(推荐使用)：解题思路：使用hash表的方式。定义一个map，存放元素和对应的下标，如果map中包含（target-当前元素），则返回他们的下标
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     * 你可以按任意顺序返回答案。
+     * <p>
+     * 作者：华星详谈
+     *
+     * @return
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        //使用hash表的方式。定义一个map，存放元素和对应的下标，如果map中包含（target-当前元素），则返回他们的下标
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
 
     /**
      * TODO 移动零：使用双指针
